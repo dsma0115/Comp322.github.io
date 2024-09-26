b2386bbf21d82d23921ba033b292ef14074fd7a7
dsma0115 <desmak0115@icloud.com>
2024-09-26 11:51:43 -0400

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,33 +1,3 @@
-
-<div>
-    <input type="number" id="number1_1" placeholder="Number 1">
-    <input type="number" id="number2_1" placeholder="Number 2">
-    <select id="operator1">
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value="*">*</option>
-        <option value="/">/</option>
-        <option value="%">%</option>
-    </select>
-    <button onclick="calculate(1)">Calculate</button>
-    <div id="result1"></div>
-    <div id="summary1"></div>
-</div>
-
-<div>
-    <input type="number" id="number1_2" placeholder="Number 1">
-    <input type="number" id="number2_2" placeholder="Number 2">
-    <select id="operator2">
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value="*">*</option>
-        <option value="/">/</option>
-        <option value="%">%</option>
-    </select>
-    <button onclick="calculate(2)">Calculate</button>
-    <div id="result2"></div>
-    <div id="summary2"></div>
-</div>
 function calculate(setNumber) {
     // Get the input values based on the set number (1 or 2)
     const number1 = parseFloat(document.getElementById(`number1_${setNumber}`).value);
